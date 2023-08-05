76b74d0c016f6d48734132ae920d63a661e80a66
tanayvaswani <vaswani.tanay9@gmail.com>
2023-08-05 20:36:27 +0530

    added the required changes to run the server

--- server/main.go
+++ server/main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"log"
+	"net"
+	pb "github.com/tanayvaswani/grpc-impl-go/proto"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":8080"
+)
+
+type helloServer struct {
+	pb.GreetServiceServer
+} 
+
+func main() {
+	lisn, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("Failed to start the server: %v", err)
+	}
+
+	// gRPC Server 
+	grpcServer := grpc.NewServer()
+	pb.RegisterGreetServiceServer(grpcServer, &helloServer{})
+	if err := grpcServer.Serve(lisn); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
+}
